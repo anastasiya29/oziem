57131763fa75eebd0e6f404af0c7575fa1461e53
Flynn <flynn.github@gmail.com>
2024-01-26 17:08:28 -0500

    Reorder imports alphabetically

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Head from '@components/Head'
 import Banner from '@components/Banner'
 import Border from '@components/Border'
+import Footer from '@components/Footer'
+import Head from '@components/Head'
 import Links from '@components/Links'
 import Shows from '@components/Shows'
 import Video from '@components/Video'
-import Footer from '@components/Footer'
 
 export default function Home() {
 
