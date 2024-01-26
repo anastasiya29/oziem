dbfbad33021b3cc89bb9666e4d35ea28a2fd7108
Flynn <flynn.github@gmail.com>
2024-01-26 16:32:35 -0500

    Footer - add background image

--- components/Footer.js
+++ components/Footer.js
@@ -2,11 +2,9 @@ import styles from './Footer.module.scss'
 
 export default function Footer() {
   return (
-    <>
-      <footer className={styles.footer}>
-        <div>© {(new Date().getFullYear())} OZIEM</div>
-        <div>We do not use cookies and we do not track you</div>
-      </footer>
-    </>
+    <footer className={styles.footer}>
+      <div className={styles.copyright}>© {(new Date().getFullYear())} OZIEM</div>
+      <div>We do not use cookies and we do not track you</div>
+    </footer>
   )
 }
